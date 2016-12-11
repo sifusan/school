af484b96315a1b27249fab1efca1a4462388455e
sifusan <fuglestad.simen@gmail.com>
2016-12-11 18:45:13 +0000

    removed comment

--- ce153_databases/assignment/query.sql
+++ ce153_databases/assignment/query.sql
@@ -12,5 +12,3 @@ SELECT first_name, last_name, salary FROM author WHERE salary = (select max(sala
 SELECT first_name, last_name FROM author WHERE ni_number IN (select author from writes);
 
 SELECT DISTINCT article.title, article.topic, author.first_name, author.last_name FROM article, author, writes WHERE author.ni_number = (SELECT author FROM writes WHERE writes.article=article.articleID) AND (article.content LIKE '%DAVID CAMERON%' OR article.content LIKE '%George Clooney%');
-
--- SELECT article.title, article.topic, author.first_name, author.last_name FROM article, author WHERE writes.author = author. article.content LIKE '%David Cameron%';
