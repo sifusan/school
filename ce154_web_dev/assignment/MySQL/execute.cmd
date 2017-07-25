78ab77e83d44315a25781c61132b4c8b418e8251
sifusan <fuglestad.simen@gmail.com>
2017-07-25 20:14:09 +0100

    cleanup

--- ce154_web_dev/assignment/MySQL/execute.cmd
+++
@@ -1,8 +0,0 @@
-@echo off
-
-rem If you want the "interactive output" format in batch mode, use mysql -t
-rem To echo to the output the commands that are executed, use mysql -vvv
-rem for example:
-rem mysql -t -vvv -u root -p < script.sql > log.txt
-
-c:\mysql\bin\mysql -t -vvv -u root < script.sql > log.txt
--- ce154_web_dev/assignment/invalid_review.jsp
+++
@@ -1,9 +0,0 @@
-<!DOCTYPE html>
-<html>
-  <head>
-    <meta charset="UTF-8">
-    <title>Invalid Review</title>
-  </head>
-  <body>
-    <h1> No rating selected, please try again </h1>
-  </body>
