f4ea318adf630dfd1eb98764dfac440d77b3f82a
sifusan <fuglestad.simen@gmail.com>
2017-10-06 00:43:37 +0200

    added paring for let

--- inf122/Oblig1.hs
+++ inf122/Oblig1.hs
@@ -5,69 +5,68 @@ import Data.Char
 import Data.List.Split
 import Data.List
 
-{-
 
+{-
 equals_ :: Parser ()
 equals_ [] = error "equals_: expcted = but got end of input"
 equals_ ("=":ss) = ((),ss)
 equals_ (s:ss) = error $ "equals_: expected = but got "++s
-
-parseExpr :: [String] -> (Ast, [String])
-parseExpr []                     = error "invalid input"
-parseExpr (x:xs) | all isDigit x = (Nr (read x), xs)
-                 | x == "-"      = (Min (fst(parseExpr xs)), xs)
-
-                 | x == "+"      = (Sum (fst(parseExpr xs))
-                                        (fst(parseExpr(snd(parseExpr xs)))),
-                                        xs)
-
-                 | x == "*"      = (Mul (fst(parseExpr xs))
-                                        (fst(parseExpr(snd(parseExpr xs)))),
-                                        xs)
 -}
-data Ast = Nr Int | Sum Ast Ast | Mul Ast Ast | Min Ast
-                  | If Ast Ast Ast deriving (Eq, Show)
 
-mapParser :: (Ast -> Ast) ->
-                ([String] -> (Ast, [String])) ->
-                ([String] -> (Ast, [String]))
-mapParser f p = \x -> let
-                        (a,y) = p x
-                        in (f a,y)
+parseExpr' :: [String] -> (Ast, [String])
+parseExpr' []                     = error "invalid input"
+parseExpr' (x:xs) | all isDigit x = (Nr (read x), xs)
+                 | x == "-"      = (Min (fst(parseExpr' xs)), xs)
 
---SPØR OM parseExpr må ta String og ikke [String]!!!!!!!!!!
+                 | x == "+"      = (Sum (fst(parseExpr' xs))
+                                        (fst(parseExpr'(snd(parseExpr' xs)))),
+                                        xs)
+
+                 | x == "*"      = (Mul (fst(parseExpr' xs))
+                                        (fst(parseExpr'(snd(parseExpr' xs)))),
+                                        xs)
+
+data Ast = Nr Int | Var String | Sum Ast Ast | Mul Ast Ast | Min Ast
+                  | If Ast Ast Ast | Let String Ast Ast  deriving (Eq, Show)
+
+
 parseExpr :: [String] -> (Ast,[String])
 parseExpr [] = error "invalid input"
-parseExpr (x:xs)   | all isDigit x = (Nr (read x),xs)
-                    | x == "-" = mapParser Min parseExpr xs
-                    | x == "+" = liftParser Sum parseExpr parseExpr xs
-                    | x == "*" = liftParser Mul parseExpr parseExpr xs
-                    | x == "if"= liftParser' If parseExpr parseExpr parseExpr xs
+parseExpr (x:xs) | all isDigit x = (Nr (read x), xs)
+                 | x == "-"      = liftParser1 Min parseExpr xs
+                 | x == "+"      = liftParser2 Sum parseExpr parseExpr xs
+                 | x == "*"      = liftParser2 Mul parseExpr parseExpr xs
+                 | x == "if"     = liftParser3 If parseExpr parseExpr parseExpr xs
+                 | x == "let"    = parseLet Let (head(xs)) parseExpr parseExpr (tail (xs))
+                 | isLetter(head x) && isUpper (head x) = (Var x, xs)
 
-liftParser :: (Ast -> Ast -> Ast) ->
-                ([String] -> (Ast, [String])) ->
-                ([String] -> (Ast, [String])) ->
-                ([String] -> (Ast, [String]))
-liftParser f p1 p2 = \x -> let
-                            (a,y) = p1 x
+type Parser a = [String] -> (a, [String])
+
+parseLet :: (String -> a -> b -> c) -> String -> Parser a -> Parser b -> Parser c
+parseLet f s p1 p2 x = let (a,y) = p1 x
+                           (b,z) = p2 y
+                           in (f s a b, z)
+
+liftParser1 :: (a -> b) -> Parser a -> Parser b
+liftParser1 f p x = let (a,y) = p x in (f a,y)
+
+liftParser2 :: (a -> b -> c) -> Parser a -> Parser b -> Parser c
+liftParser2 f p1 p2 x = let (a,y) = p1 x
                             (b,z) = p2 y
                             in (f a b, z)
-
-liftParser' :: (Ast -> Ast -> Ast -> Ast) ->
-                ([String] -> (Ast, [String])) ->
-                ([String] -> (Ast, [String])) ->
-                ([String] -> (Ast, [String])) ->
-                ([String] -> (Ast, [String]))
-liftParser' f p1 p2 p3 = \x -> let
-                                (a, y) = p1 x
-                                (b, z) = p2 y
-                                (c, w) = p3 z
-                                in (f a b c, w)
+--
+liftParser3 :: (a -> b -> c -> d) -> Parser a -> Parser b -> Parser c -> Parser d
+liftParser3 f p1 p2 p3 x = let (a, y) = p1 x
+                               (b, z) = p2 y
+                               (c, w) = p3 z
+                               in (f a b c, w)
 
 cleanString :: [String] -> [String]
 cleanString [] = []
 cleanString xs |  (head xs == ",") ||
-                  (head xs == "")       = cleanString (drop 1 xs)
+                  (head xs == "")  ||
+                  (head xs == "=") ||
+                  (head xs == "in")     = cleanString (drop 1 xs)
                | otherwise              = head xs : cleanString(tail xs)
 
 tokenize :: String -> [String]
@@ -90,6 +89,8 @@ realJob (Mul x y)   = (fst(realJob x) * fst(realJob y),
 
 realJob (If x y z)  | fst(realJob x) == 0 = realJob y
                     | otherwise           = realJob z
+
+realJob (Let s x y) = (fst(realJob(x)), (snd(realJob(x))))
 
 evi :: String -> Int
 evi s = fst(realJob(parse s))
