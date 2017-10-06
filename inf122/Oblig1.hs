0d18d3e46e720366f59c373523015eaab10d7961
sifusan <fuglestad.simen@gmail.com>
2017-10-06 11:45:23 +0200

    removed obsolete realjob

--- inf122/Oblig1.hs
+++ inf122/Oblig1.hs
@@ -1,34 +1,22 @@
 --Name: Simen Fuglestad
-
 --module Oblig1 where
 import Data.Char
 import Data.List.Split
 import Data.List
 
-
-{-
-equals_ :: Parser ()
-equals_ [] = error "equals_: expcted = but got end of input"
-equals_ ("=":ss) = ((),ss)
-equals_ (s:ss) = error $ "equals_: expected = but got "++s
--}
+data Ast = Nr Int | Var String | Sum Ast Ast | Mul Ast Ast | Min Ast
+                  | If Ast Ast Ast | Let String Ast Ast  deriving (Eq, Show)
 
 parseExpr' :: [String] -> (Ast, [String])
 parseExpr' []                     = error "invalid input"
 parseExpr' (x:xs) | all isDigit x = (Nr (read x), xs)
                  | x == "-"      = (Min (fst(parseExpr' xs)), xs)
-
                  | x == "+"      = (Sum (fst(parseExpr' xs))
                                         (fst(parseExpr'(snd(parseExpr' xs)))),
                                         xs)
-
                  | x == "*"      = (Mul (fst(parseExpr' xs))
                                         (fst(parseExpr'(snd(parseExpr' xs)))),
                                         xs)
-
-data Ast = Nr Int | Var String | Sum Ast Ast | Mul Ast Ast | Min Ast
-                  | If Ast Ast Ast | Let String Ast Ast  deriving (Eq, Show)
-
 
 parseExpr :: [String] -> (Ast,[String])
 parseExpr [] = error "invalid input"
@@ -63,9 +51,7 @@ liftParser3 f p1 p2 p3 x = let (a, y) = p1 x
 
 cleanString :: [String] -> [String]
 cleanString [] = []
-cleanString xs |  (head xs == ",") ||
-                  (head xs == "")  ||
-                  (head xs == "=") ||
+cleanString xs |  (head xs == ",") || (head xs == "")  || (head xs == "=") ||
                   (head xs == "in")     = cleanString (drop 1 xs)
                | otherwise              = head xs : cleanString(tail xs)
 
@@ -75,44 +61,24 @@ tokenize xs = cleanString(splitOn " " xs)
 parse :: String -> Ast
 parse xs =  fst(parseExpr(tokenize xs))
 
---Memory in here somewhere!
-realJob :: Ast -> (Int, Bool)
-realJob (Nr n)      | n `mod` 2 /= 0 = (n, True)
-                    | otherwise = (n, False)
-
-realJob (Sum x y)   = (fst(realJob x) + fst(realJob y),
-                        snd(realJob x) || snd(realJob y))
-
-realJob (Min x)     = (- fst(realJob x), not(snd(realJob x)))
-
-realJob (Mul x y)   = (fst(realJob x) * fst(realJob y),
-                        snd(realJob x) && snd(realJob y))
-
-realJob (If x y z)  | fst(realJob x) == 0 = realJob y
-                    | otherwise           = realJob z
 
 realJob' :: Ast -> [(String, Int)] -> (Int, Bool)
-realJob' (Nr n) xs      | n `mod` 2 /= 0 = (n, True)
-                        | otherwise = (n, False)
-
+realJob' (Nr n) xs      | even n = (n, False)
+                        | odd n = (n, True)
 realJob' (Sum x y) xs   = (fst(realJob' x xs)   + fst(realJob' y xs),
                             snd(realJob' x xs) || snd(realJob' y xs))
 
 realJob' (Min x) xs     = (- fst(realJob' x xs), not(snd(realJob' x xs)))
-
 realJob' (Mul x y) xs   = (fst(realJob' x xs)   * fst(realJob' y xs),
                             snd(realJob' x xs) && snd(realJob' y xs))
 
 realJob' (If x y z) xs  | fst(realJob' x xs) == 0   = realJob' y xs
                         | otherwise                 = realJob' z xs
 
-
-realJob' (Let s x y) xs = realJob' y ((s, fst(realJob(x))) : xs)
-
+realJob' (Let s x y) xs = realJob' y ((s, (fst(realJob' x xs))) : xs)
 realJob' (Var s) xs     = realJob' (Nr (findBoundValue s xs)) xs
 
 findBoundValue :: String -> [(String, Int)]-> Int
-findBoundValue _ [] = error "variable not bound"
 findBoundValue s (x:xs) | s == fst(x)   = snd(x)
                         | otherwise     = findBoundValue s xs
 
