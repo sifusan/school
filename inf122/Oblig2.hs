acad104c5f88810643439a6523dcadfdbb786417
sifusan <fuglestad.simen@gmail.com>
2017-11-16 01:43:01 +0100

    added survivors

--- inf122/Oblig2.hs
+++ inf122/Oblig2.hs
@@ -5,7 +5,7 @@ import Data.List
 import System.Exit;
 
 commands :: [String]
-commands = ["c", "help", "q", "n", "d"]
+commands = ["c", "help", "q", "n", "d", "s", "b", "\CR", ""]
 
 isCommand :: String -> [String] -> Bool
 isCommand xs []   = False
@@ -50,7 +50,7 @@ funkyShit l x y (ns:nss)
     | x == l && y == l                          = "X\n"
     | (x == x1 && y == y1) && x /= l && y > 8   = "X" ++ "  " ++ funkyShit l (x+1) y nss
     | (x == x1 && y == y1) && (x /= l) &&
-        (x == 1 && y == 1)                      = show y ++ "  X  " ++ funkyShit l (x+1) y nss
+      (x == 1 && y == 1)                        = show y ++ "  X  " ++ funkyShit l (x+1) y nss
     | (x == x1 && y == y1) && x /= l            = "X  "  ++ funkyShit l (x+1) y nss
     | (x == x1 && y == y1) && (x == l && y > 8) = "X\n" ++ show y ++ " " ++ funkyShit l 1 (y+1) nss
     | (x == x1 && y == y1) && x == l            = "X\n" ++ show y ++ "  " ++ funkyShit l 1 (y+1) nss
@@ -62,7 +62,8 @@ funkyShit l x y (ns:nss)
     | otherwise = error "Something really strange happened"
     where
         x1 = head ns
-        y1 = head (tail ns)
+        y1 = ns !! 1
+
 
 numbersToChars :: [Int] -> String
 numbersToChars []       = ""
@@ -72,8 +73,9 @@ numbersToChars (n:ns)
     | otherwise = show n ++ " " ++ numbersToChars ns
 
 createWithCells :: Int -> [[Int]] -> IO ()
+createWithCells n [] = create n
 createWithCells n cells = do
-    putStrLn ("  " ++ numbersToChars [1..n])
+    putStrLn ("   " ++ numbersToChars [1..n])
     putStr (funkyShit n 1 1 (rmdups(filterEmpty(sortbyYvalue cells))))
 
 cellInRange :: [Int] -> Int -> Bool
@@ -82,7 +84,7 @@ cellInRange cell l | ((0 < x) && (x <= l)) && ((0 < y) && (y <= l)) = True
                    | otherwise = False
                    where
                        x = head cell
-                       y = head (tail cell)
+                       y = cell !! 1
 
 dropAndCreateWithCells :: Int -> [Int] -> [[Int]] -> IO ()
 dropAndCreateWithCells n d cells = do
@@ -97,61 +99,101 @@ create n = do
 cls :: IO ()
 cls = putStr "\ESC[2J"
 
---use first paramater to save height and width of board
---use second parameter to save list of cells
---bool is flag to determine if the loop is running for the first time
 
 dropCell :: [Int] -> [[Int]] -> [[Int]]
 dropCell d [] = []
 dropCell d (ns:nss) | d /= ns = dropCell d nss ++ [ns]
                     | otherwise = nss
 
-executionLoop :: Int -> [[Int]] -> Bool -> IO ()
+
+adjacentCells :: [Int] -> [[Int]] -> [[Int]]
+adjacentCells _ [[]]        = []
+adjacentCells _ []          = []
+adjacentCells cell (ns:nss)
+    | x1 == x && y1 == y = adjacentCells cell nss
+    | (x1 == x - 1 || x1 == x + 1 || x1 == x) &&
+      (y1 == y - 1 || y1 == y + 1 || y1 == y) = ns : adjacentCells cell nss
+    | otherwise = adjacentCells cell nss
+    where
+        x1 = head ns
+        y1 = ns !! 1
+        x = head cell
+        y = cell !! 1
+
+survives :: [Int] -> [[Int]] -> (Int,Int) -> Bool
+survives cell cells s | l >= fst s && l <= snd s = True
+                      | otherwise = False
+                      where
+                          l = length (adjacentCells cell cells)
+
+survivors :: [[Int]] -> (Int, Int) -> [[Int]]
+survivors cells s = rmdups [x | x <- cells, survives x cells s]
+                    --where s1 = ((fst s)-1, (snd s)-1)
+
+executionLoop :: Int -> [[Int]] -> (Int, Int) -> (Int, Int) -> Bool -> IO ()
 --executionLoop _ [] _  = exitFailure
-executionLoop n ns False = do
+executionLoop n ns ss bs False = do
     putStrLn "enter a command"
     input <- getLine
-    if isCommand (head (splitInput input)) commands then
-        let i1 = head (formatArguments(tail(splitInput input)))
-            i2 = formatArguments(tail (splitInput input)) in
-        case head input of
-            'q' -> exitSuccess
-            'c' -> create i1
-            'n' -> do
-                putStrLn "No board created yet, unable to put new cell"
-                executionLoop 0 ns False
-            'd' -> do
-                putStrLn "No board creaed yet, unable to drop"
-                executionLoop 0 ns False
-    else putStrLn "invalid command"
-    executionLoop (head(formatArguments(tail(splitInput input)))) ns True
+    if input == "" then do
+            putStrLn "No board created yet, unable to step forward one"
+            executionLoop n ns ss bs False
+    else
+        if isCommand (head (splitInput input)) commands then
+            let i1 = head (formatArguments(tail(splitInput input)))
+                i2 = formatArguments(tail (splitInput input)) in
+            case [head input] of
+                "q" -> exitSuccess
+                "c" -> create i1
+                "n" -> do
+                    putStrLn "No board created yet, unable to put new cell"
+                    executionLoop 0 ns ss bs False
+                "d" -> do
+                    putStrLn "No board created yet, unable to drop"
+                    executionLoop 0 ns ss bs False
+                "s" -> do
+                    putStrLn (show i1 ++ " " ++ show (i2 !! 1))
+                    executionLoop n ns (i1, i2 !! 1) bs False
+                "b" -> do
+                    putStrLn (show i1 ++ " " ++ show (i2 !! 1))
+                    executionLoop n ns ss (i1, i2 !! 1) False
+        else putStrLn "Invalid command"
+    executionLoop (head(formatArguments(tail(splitInput input)))) ns ss bs True
 
-executionLoop n ns True = do
+executionLoop n ns ss bs True = do
     putStrLn "enter a command"
     input <- getLine
-    if isCommand (head(splitInput input)) commands then
-        let i1 = head (formatArguments(tail(splitInput input)))
-            i2 = formatArguments(tail (splitInput input)) in
-        case head input of
-            'q' -> exitSuccess
-            'c' -> do
-                create i1
-                executionLoop i1 ns True
-            'n' -> do
-                if cellInRange [i1, i2 !! 1] n then
-                    do
-                        createWithCells n ([[i1, i2 !! 1]] ++ ns)
-                        executionLoop n (i2:ns) True
-                else
-                    do
-                        putStrLn "Invalid cell"
-                        createWithCells n ns
-                executionLoop n ns True
+    if input == "" then do
+            createWithCells n (survivors ns ss)
+            executionLoop n (survivors ns ss) ss bs True
+    else
+        if isCommand (head(splitInput input)) commands then
+            let i1 = head (formatArguments(tail(splitInput input)))
+                i2 = formatArguments(tail (splitInput input)) in
+                case head input of
+                    'q' -> exitSuccess
+                    'c' -> do
+                        create i1
+                        executionLoop i1 ns ss bs True
+                    'n' -> do
+                        if cellInRange [i1, i2 !! 1] n then
+                            do
+                                createWithCells n ([[i1, i2 !! 1]] ++ ns)
+                                executionLoop n (i2:ns) ss bs True
+                        else
+                            do
+                                putStrLn "Invalid cell"
+                                createWithCells n ns
+                        executionLoop n ns ss bs True
+                    'd' -> do
+                        dropAndCreateWithCells n [i1, i2 !! 1] ns
+                        executionLoop n (dropCell [i1, i2 !! 1] ns) ss bs True
+                    's' -> do
+                        executionLoop n ns (i1, i2 !! 1) bs True
+                    'b' -> do
+                        putStrLn (show i1 ++ " " ++ show (i2 !! 1))
+                        executionLoop n ns ss (i1, i2 !! 1) True
+        else putStrLn "invalid command"
+    executionLoop n ns ss bs True
 
-            'd' -> do
-                dropAndCreateWithCells n [i1, i2 !! 1] ns
-                executionLoop n (dropCell [i1, i2 !! 1] ns) True
-    else putStrLn "invalid command"
-    executionLoop n ns True
-
-main = executionLoop 0 [[]] False
+main = executionLoop 0 [[]] (-1,-1) (-1,-1) False
