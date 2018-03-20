fd8d54aadb20764930ae0241a9fdf01377bc1e8a
sifusan <fuglestad.simen@gmail.com>
2018-03-20 16:40:21 +0100

    server now accepts paths from client

--- inf142/Oblig1/WPS.java
+++ inf142/Oblig1/WPS.java
@@ -1,4 +1,3 @@
-import com.sun.scenario.effect.impl.sw.sse.SSEBlend_SRC_OUTPeer;
 
 import java.net.*;
 import java.io.*;
@@ -8,39 +7,30 @@ import java.io.*;
  */
 public class WPS {
     private static final int portNumber = 12000;
-    private static int clientPort;
-    private static InetAddress clientIP;
-    private static DatagramSocket socket;
 
     public static void main(String[] args) throws IOException {
-        try  {
-            socket = new DatagramSocket(portNumber);
-            byte[] receiveData = new byte[1024];
-            byte[] sendData;
-
-            while (true) {
-                DatagramPacket receivePacket =
-                        new DatagramPacket(receiveData, receiveData.length);
-                socket.receive(receivePacket);
-                String userRequestedPage = new String(receivePacket.getData());
-                System.out.println("User requested page at \"" + userRequestedPage+"\"");
-                clientIP = receivePacket.getAddress();
-                clientPort = receivePacket.getPort();
-                System.out.println("Client port is " + clientPort);
-                sendToClient(queryWeb(userRequestedPage), socket, clientIP, clientPort);
+        byte[] receiveData = new byte[1024];
+        byte[] sendData = new byte[1024];
+        try {
+            DatagramSocket serverSocket = new DatagramSocket(portNumber);
+            while(true) {
+                DatagramPacket gotPacket = new DatagramPacket(receiveData, receiveData.length);
+                serverSocket.receive(gotPacket);
+                String message = new String(gotPacket.getData(), 0, gotPacket.getLength());
+                String[] splitted = message.split("/", 2);
+                String result;
+                if (splitted.length > 1) {
+                    result = queryWeb(splitted[0], "/"+splitted[1]);
+                } else {
+                    result = queryWeb(splitted[0], "/");
+                }
+                sendToClient(result, serverSocket, gotPacket.getAddress(), gotPacket.getPort());
             }
-
-        } catch (IOException | NullPointerException e) {
-
-            if (e instanceof  UnknownHostException) {
-                sendToClient("The server could not locate a web page by that name", socket, clientIP, clientPort);
-            }
-            else {
-                sendToClient("An error occured on the server", socket, clientIP, clientPort);
-            }
+        } catch(IOException e) {
             e.printStackTrace();
         }
     }
+
     private static void sendToClient(String message, DatagramSocket socket, InetAddress ip, int port) {
         try {
             byte[] sendData;
@@ -53,21 +43,25 @@ public class WPS {
         }
     }
 
-    private static String queryWeb(String userRequestPage) throws IOException {
-        InetAddress requestedWebIP = InetAddress.getByName(userRequestPage);
+    private static String queryWeb(String host, String path) throws IOException {
+        InetAddress requestedWebIP = InetAddress.getByName(host);
+        System.out.println(requestedWebIP.toString());
         try (
                 Socket toWebSocket = new Socket(requestedWebIP, 80);
-                BufferedOutputStream bs = new BufferedOutputStream(toWebSocket.getOutputStream());
-                BufferedInputStream is = new BufferedInputStream(toWebSocket.getInputStream());
+                BufferedOutputStream outPutStream = new BufferedOutputStream(toWebSocket.getOutputStream());
+                BufferedReader inputStream = new BufferedReader(new InputStreamReader(toWebSocket.getInputStream()))
         ) {
 
-            String request = "GET / HTTP/1.0\r\n\r\n";
-            bs.write(request.getBytes());
-            bs.flush();
+            System.out.println("HOST IS: " + host);
+            System.out.println("PATH IS: " + path);
+            String request = "HEAD " + path + " HTTP/1.1\r\n" + "Host: " + host + "\r\n\r\n";
+            outPutStream.write(request.getBytes());
+            outPutStream.flush();
+            String input;
             String result = "";
-            int ch;
-            while ((ch = is.read()) != -1) {
-                result = result + ((char) ch);
+
+            while ((input = inputStream.readLine()) != null) {
+                result = result + input + "\n";
             }
 
             return result;
