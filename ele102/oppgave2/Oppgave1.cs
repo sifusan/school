967bd04da9ddb5732f831835a18f730f35bbf479
sifusan <fuglestad.simen@gmail.com>
2019-02-20 12:51:58 +0100

    added tasks 1-3

---
+++ ele102/oppgave2/Oppgave1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace O1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a1, a2;
+            char operasjon;
+
+            Console.WriteLine("Oppgi to heltall"); // endret writeLine til WriteLine
+            Console.Write("Tall 1: ");
+            a1 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Tall 2: ");
+            a2 = Convert.ToInt32(Console.ReadLine()); // endret ToDouble til ToInt32, oppgaven spesifiserer heltall og ikke double
+            Console.WriteLine("Oppgi ønsket operasjon: ");
+            Console.Write("Operasjoner [+,*]:");
+            operasjon = Convert.ToChar(Console.ReadLine());
+
+            if (operasjon == '+') // endret "+" til '+', operasjon er av type char og ikke string
+            {
+                int resultat = a1 + a2; //la til type deklarasjon, resultat er ikke deklarert tidligere
+                Console.Write(a1.ToString() + " + " + a2.ToString());
+                Console.WriteLine(" = " + resultat.ToString());
+
+            }
+            else if (operasjon == '*') //2 endringer: 1. endret "*" til '*', må ha char. 2. Bytttet plass på else if og else setninger
+            {
+                int resultat = a1 * a2; //2 endringer: La til type deklarasjon og fjernet single quotes
+                Console.Write("{0}, * {1}", a1, a2);
+                Console.WriteLine(" = {0}", resultat);
+            }
+            else
+            {
+                Console.WriteLine("Uglydig operasjon oppgitt");
+            }
+            Console.WriteLine("Trykk på en knapp for å avslutte...");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ ele102/oppgave2/Oppgave2_v2/Oppgave2_v2/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+namespace Oppgave2_v2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+
+                Console.OutputEncoding = System.Text.Encoding.UTF8;
+                double a = Convert.ToDouble(Console.ReadLine());
+                double b = Convert.ToDouble(Console.ReadLine());
+                double c = Convert.ToDouble(Console.ReadLine());
+                double squareMe = Math.Pow(b, 2) - (4 * a * c);
+
+                if (a == 0 && b == 0 && c == 0)
+                {
+                    Console.WriteLine("Det er evig mange løsninger");
+                }
+                else if (a == 0 && b != 0 && c == 0)
+                {
+                    Console.WriteLine("En løsning: x = 0");
+                }
+                else if (a == 0 && b == 0 && c != 0)
+                {
+                    Console.WriteLine("Det er ingen løsninger");
+                }
+                else if (a == 0 && b != 0 && c != 0)
+                {
+                    Console.WriteLine("Løsning er linær: x1 = x2 =" + (-b / c));
+                }
+                else if (squareMe < 0)
+                {
+                    double x = Math.Sqrt(Math.Abs(squareMe)) / (2 * a);
+                    string bStr = (-b / (2 * a)).ToString("0.000");
+                    Console.WriteLine("To løsninger: x1 = " + bStr + " + " + x.ToString("0.000") + "i, x2 = " + bStr + " - " + x.ToString("0.000") + "i");
+                }
+                else
+                {
+                    double x1 = (-b + Math.Sqrt(squareMe)) / (2 * a);
+                    double x2 = (-b - Math.Sqrt(squareMe)) / (2 * a);
+
+                    if (x1 == x2)
+                    {
+                        Console.WriteLine("x1 = x2 = " + x1.ToString("0.000"));
+                    }
+                    else
+                    {
+                        Console.WriteLine("x1 = " + x1.ToString("0.000") + "\nx2 = " + x2.ToString("0.000"));
+                    }
+                }
+                Console.ReadKey();
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                Console.ReadKey();
+            }
+        }
+    }
+}
