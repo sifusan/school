b8827f901ca51f83eb170c22a569dd66c42c224f
sifusan <fuglestad.simen@gmail.com>
2019-04-14 20:19:50 +0200

    finished task 4

---
+++ ele102/oppgave5/O4/O4/Form1.Designer.cs
@@ -0,0 +1,133 @@
+﻿namespace O4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.start_btn = new System.Windows.Forms.Button();
+            this.stop_btn = new System.Windows.Forms.Button();
+            this.clear_txt_btn = new System.Windows.Forms.Button();
+            this.exit_btn = new System.Windows.Forms.Button();
+            this.port_select_box = new System.Windows.Forms.ComboBox();
+            this.last_rec_msg_box = new System.Windows.Forms.TextBox();
+            this.last_ten_rec_msg_box = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // start_btn
+            // 
+            this.start_btn.Location = new System.Drawing.Point(12, 36);
+            this.start_btn.Name = "start_btn";
+            this.start_btn.Size = new System.Drawing.Size(104, 44);
+            this.start_btn.TabIndex = 0;
+            this.start_btn.Text = "Start";
+            this.start_btn.UseVisualStyleBackColor = true;
+            this.start_btn.Click += new System.EventHandler(this.start_btn_click);
+            // 
+            // stop_btn
+            // 
+            this.stop_btn.Location = new System.Drawing.Point(436, 36);
+            this.stop_btn.Name = "stop_btn";
+            this.stop_btn.Size = new System.Drawing.Size(111, 44);
+            this.stop_btn.TabIndex = 1;
+            this.stop_btn.Text = "Stopp";
+            this.stop_btn.UseVisualStyleBackColor = true;
+            this.stop_btn.Click += new System.EventHandler(this.stop_btn_click);
+            // 
+            // clear_txt_btn
+            // 
+            this.clear_txt_btn.Location = new System.Drawing.Point(553, 36);
+            this.clear_txt_btn.Name = "clear_txt_btn";
+            this.clear_txt_btn.Size = new System.Drawing.Size(111, 44);
+            this.clear_txt_btn.TabIndex = 2;
+            this.clear_txt_btn.Text = "Fjern Text";
+            this.clear_txt_btn.UseVisualStyleBackColor = true;
+            this.clear_txt_btn.Click += new System.EventHandler(this.clear_txt_btn_click);
+            // 
+            // exit_btn
+            // 
+            this.exit_btn.Location = new System.Drawing.Point(670, 36);
+            this.exit_btn.Name = "exit_btn";
+            this.exit_btn.Size = new System.Drawing.Size(111, 44);
+            this.exit_btn.TabIndex = 3;
+            this.exit_btn.Text = "Avslutt";
+            this.exit_btn.UseVisualStyleBackColor = true;
+            this.exit_btn.Click += new System.EventHandler(this.exit_btn_click);
+            // 
+            // port_select_box
+            // 
+            this.port_select_box.FormattingEnabled = true;
+            this.port_select_box.Location = new System.Drawing.Point(242, 48);
+            this.port_select_box.Name = "port_select_box";
+            this.port_select_box.Size = new System.Drawing.Size(121, 21);
+            this.port_select_box.TabIndex = 4;
+            // 
+            // last_rec_msg_box
+            // 
+            this.last_rec_msg_box.Location = new System.Drawing.Point(12, 103);
+            this.last_rec_msg_box.Name = "last_rec_msg_box";
+            this.last_rec_msg_box.Size = new System.Drawing.Size(750, 20);
+            this.last_rec_msg_box.TabIndex = 5;
+            // 
+            // last_ten_rec_msg_box
+            // 
+            this.last_ten_rec_msg_box.Location = new System.Drawing.Point(12, 148);
+            this.last_ten_rec_msg_box.Name = "last_ten_rec_msg_box";
+            this.last_ten_rec_msg_box.Size = new System.Drawing.Size(750, 244);
+            this.last_ten_rec_msg_box.TabIndex = 6;
+            this.last_ten_rec_msg_box.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.last_ten_rec_msg_box);
+            this.Controls.Add(this.last_rec_msg_box);
+            this.Controls.Add(this.port_select_box);
+            this.Controls.Add(this.exit_btn);
+            this.Controls.Add(this.clear_txt_btn);
+            this.Controls.Add(this.stop_btn);
+            this.Controls.Add(this.start_btn);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button start_btn;
+        private System.Windows.Forms.Button stop_btn;
+        private System.Windows.Forms.Button clear_txt_btn;
+        private System.Windows.Forms.Button exit_btn;
+        private System.Windows.Forms.ComboBox port_select_box;
+        private System.Windows.Forms.TextBox last_rec_msg_box;
+        private System.Windows.Forms.RichTextBox last_ten_rec_msg_box;
+    }
+}
+
---
+++ ele102/oppgave5/O4/O4/Form1.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO.Ports;
+using System.Windows.Forms;
+
+namespace O4
+{
+    public partial class Form1 : Form
+    {
+        SerialPort serial_port;
+        bool started = false;
+        const int BAUD_RATE = 9600;
+        bool exit_flag = false;
+        Timer timer = new Timer();
+        string[] last_ten_msgs = new string[10];
+        int nr_msgs = 0;
+        public Form1()
+        {
+            InitializeComponent();
+            string[] port_names = SerialPort.GetPortNames();
+            for (int i = 0; i <port_names.Length; i++)
+            {
+               port_select_box.Items.Add(port_names[i]);
+            }
+            timer.Tick += new EventHandler(read_data);
+            timer.Interval = 1000;
+            timer.Enabled = false;
+        }
+
+        private void start_btn_click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (!started)
+                {
+                    started = true;
+                    serial_port = new SerialPort(port_select_box.SelectedItem.ToString(), BAUD_RATE);
+
+                    serial_port.Open();
+
+                    if (serial_port.IsOpen)
+                    {
+                        timer.Start();
+                    }
+                }
+            }
+            catch (Exception exception)
+            {
+            }
+        }
+
+        private void stop_btn_click(object sender, EventArgs e)
+        {
+            timer.Stop();
+            serial_port.Close();
+            started = false;
+        }
+           
+
+        private void read_data(object sender, EventArgs e)
+        {
+            String s = serial_port.ReadLine();
+            last_rec_msg_box.Text = s;
+            insert_row_msg(s);
+
+        }
+
+        private void insert_row_msg(String new_msg)
+        {
+            
+            if (nr_msgs >= 10)
+            {
+                string[] copy = new string[10];
+                copy[0] = new_msg;
+                for (int i=0; i < last_ten_msgs.Length-1; i++)
+                {
+                    copy[i + 1] = last_ten_msgs[i];
+                }
+                last_ten_msgs = copy;
+            } else
+            {
+                last_ten_msgs[nr_msgs] = new_msg;
+            }
+            String tmp = "";
+            foreach(string s in last_ten_msgs)
+            {
+                tmp += s + "\r\n";
+            }
+            last_ten_rec_msg_box.Text = tmp;
+            nr_msgs++;
+        }
+
+        private void exit_btn_click(object sender, EventArgs e)
+        {
+            if (serial_port.IsOpen) serial_port.Close();
+            Application.Exit();
+        }
+        private void clear_txt_btn_click(object sender, EventArgs e)
+        {
+            nr_msgs = 0;
+            last_ten_msgs = new string[10];
+            last_rec_msg_box.Text = "";
+            last_ten_rec_msg_box.Text = "";
+        }
+
+    }
+}
